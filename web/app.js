67d1ba5c4c90c2f294d6097ee3ff071b2dc6d652
Adnan Hakim <adnanhakim007@gmail.com>
2020-07-25 15:26:16 +0530

    added passport

--- web/app.js
+++ web/app.js
@@ -3,23 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const errorController = require('./controllers/error');
-
-
 
 const app = express();
 
+// Middleware
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(authRoutes);
+// Routes
+app.use('/', require('./routes/auth'));
 
 app.use(errorController.get404);
 
-app.listen(3005);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started at port ${PORT}`));
